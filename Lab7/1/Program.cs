5dd9f94cdf16dcd19bd791c44d27c991050cec76
Flowseal <sellyalegium@gmail.com>
2024-12-05 10:46:02 +0300

    Lab7

---
+++ Lab7/1/Program.cs
@@ -0,0 +1,33 @@
+﻿const string inputFilePath = "text.txt";
+await RemoveCharactersFromFileAsync(inputFilePath);
+
+async Task RemoveCharactersFromFileAsync(string filePath)
+{
+    if ( !File.Exists(filePath) )
+    {
+        Console.WriteLine("Файл не найден.");
+        return;
+    }
+
+    Console.WriteLine("Введите символы, которые необходимо удалить из текста:");
+    string charsToRemove = Console.ReadLine();
+
+    string text = await File.ReadAllTextAsync(filePath);
+    string updatedText = RemoveCharacters(text, charsToRemove);
+
+    await SaveTextToFileAsync(filePath, updatedText);
+}
+
+string RemoveCharacters(string text, string charsToRemove)
+{
+    foreach ( char c in charsToRemove )
+    {
+        text = text.Replace(c.ToString(), string.Empty);
+    }
+    return text;
+}
+
+async Task SaveTextToFileAsync(string filePath, string text)
+{
+    await File.WriteAllTextAsync(filePath, text);
+}
---
+++ Lab7/2/Program.cs
@@ -0,0 +1,96 @@
+﻿using System.Diagnostics;
+using System.Net.Http.Json;
+
+HttpClient client = new HttpClient();
+const string apiUrl = "https://dog.ceo/api/breeds/image/random";
+const int numberOfImages = 10;
+
+Console.WriteLine("Asynchronous execution:");
+await DownloadImagesAsync();
+
+Console.WriteLine("\nSynchronous execution:");
+DownloadImages();
+
+async Task DownloadImagesAsync()
+{
+    Stopwatch stopwatch = Stopwatch.StartNew();
+    Task[] tasks = new Task[numberOfImages];
+
+    for (int i = 0; i < numberOfImages; i++)
+    {
+        tasks[i] = DownloadRandomImageAsync();
+    }
+
+    Console.WriteLine("Downloading images...");
+    await Task.WhenAll(tasks);
+    stopwatch.Stop();
+    Console.WriteLine($"Asynchronous time: {stopwatch.ElapsedMilliseconds} ms");
+}
+
+async Task DownloadRandomImageAsync()
+{
+    try
+    {
+        ApiResponse response = await client.GetFromJsonAsync<ApiResponse>(apiUrl);
+        if (response.Status == "success")
+        {
+            string imageUrl = response.Message;
+            Console.WriteLine($"Starting downloading from url: {imageUrl}");
+
+            await client.GetByteArrayAsync(imageUrl);
+            Console.WriteLine($"Image from {imageUrl} downloaded successfully");
+        }
+        else
+        {
+            Console.WriteLine($"Failed to get image URL: {response}");
+        }
+    }
+    catch (HttpRequestException e)
+    {
+        Console.WriteLine($"Request error: {e.Message}");
+    }
+}
+
+void DownloadImages()
+{
+    var stopwatch = Stopwatch.StartNew();
+    Console.WriteLine("Downloading images...");
+
+    for (int i = 0; i < numberOfImages; i++)
+    {
+        DownloadRandomImage();
+    }
+
+    stopwatch.Stop();
+    Console.WriteLine($"Synchronous time: {stopwatch.ElapsedMilliseconds} ms");
+}
+
+void DownloadRandomImage()
+{
+    try
+    {
+        var response = client.GetFromJsonAsync<ApiResponse>(apiUrl).Result;
+        if (response?.Status == "success")
+        {
+            string imageUrl = response.Message;
+            Console.WriteLine($"Starting downloading from url: {imageUrl}");
+
+            client.GetByteArrayAsync(imageUrl);
+            Console.WriteLine($"Image from {imageUrl} downloaded successfully");
+        }
+        else
+        {
+            Console.WriteLine($"Failed to get image URL: {response}");
+        }
+    }
+    catch (AggregateException e)
+    {
+        Console.WriteLine($"Request error: {e.InnerException?.Message}");
+    }
+}
+
+public class ApiResponse
+{
+    public string Message { get; set; }
+    public string Status { get; set; }
+}
